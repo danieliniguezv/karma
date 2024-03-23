0d333c7eb1dca9fbc898549c1eec933d74858753
Daniel Iñiguez <danieliniguezv@MacBook-Pro-de-Daniel.local>
2024-03-23 02:16:09 -0600

    feat: added header componentsto app

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,1 +1,10 @@
-console.log(process.env.STREAMING_KEY);
+import { SongInfo } from './components/header.js';
+
+const artistName = SongInfo('Artist');
+document.getElementById('artistName').appendChild(artistName);
+
+const songName = SongInfo('Song Name');
+document.getElementById('songName').appendChild(songName);
+
+const duration = SongInfo('Duration');
+document.getElementById('duration').appendChild(duration);
