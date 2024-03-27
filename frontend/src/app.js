0ededa23cf04f67722a2e0a07a519b5c149ba8a3
Daniel Iñiguez <danieliniguezv@MacBook-Pro-de-Daniel.local>
2024-03-26 23:30:33 -0600

    feat: added imports and page context handling

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,24 +1,43 @@
 import { LoginWindowText } from './components/login.js';
-import { Connect } from './services/walletInteractions.js';
+import { Connect, AccountChanged } from './services/walletInteractions.js';
+import { TextElement } from './components/dashboard.js';
 
-const welcomeText = LoginWindowText('Welcome to Karma!');
-document.getElementById('welcome-text').appendChild(welcomeText);
+let userAddress;
+const currentPage = document.body.getAttribute('data-page');
 
-const signIn = LoginWindowText('Sign in');
-document.getElementById('sign-in').appendChild(signIn);
+AccountChanged();
 
-const connectButton = document.getElementById('metamask-button');
-connectButton.addEventListener('click', async () => {
-	try {
-		const userAddress = await Connect();
-		console.log(userAddress);
-
-		if (userAddress) {
-			window.location.href = '../public/dashboard.html';
-		} else {
-			null;
-		}
-	} catch (error) {
-		console.log('Error: ' + error.message);
+document.addEventListener('DOMContentLoaded', () => {
+	userAddress = localStorage.getItem('userAddress');
+	if (userAddress) {
+		Connect();
 	}
 });
+
+/* Login */
+if (currentPage === 'login') {
+	const welcomeText = LoginWindowText('Welcome to Karma!');
+	document.getElementById('welcome-text').appendChild(welcomeText);
+
+	const signIn = LoginWindowText('Sign in');
+	document.getElementById('sign-in').appendChild(signIn);
+
+	const connectButton = document.getElementById('metamask-button');
+	connectButton.addEventListener('click', async () => {
+		try {
+			const address = await Connect();
+			localStorage.setItem('userAddress', address);
+			if (address) {
+				window.location.href = '../public/dashboard.html';
+			} else {
+				null;
+			}
+		} catch (error) {
+			console.log('Error: ' + error.message);
+		}
+	});
+} else if (currentPage === 'dashboard') {
+	const address = localStorage.getItem('userAddress');
+	userAddress = TextElement(address);
+	document.getElementById('user-account').appendChild(userAddress);
+}
