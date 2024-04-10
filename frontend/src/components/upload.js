21ff90cd19d9360f8a2eec1fabf8666a1b3f668c
Daniel Iñiguez <danieliniguezv@MacBook-Pro-de-Daniel.local>
2024-04-09 23:07:47 -0600

    feat: added upload component

---
+++ frontend/src/components/upload.js
@@ -0,0 +1,38 @@
+export default class UploadComponent {
+	constructor(page) {
+		this.container = document.createElement('div');
+		this.container.classList.add('upload');
+		this.container.setAttribute('data-page', page);
+		this.container.innerHTML = `
+			<div class="input-fields" data-page="${page}">
+				<div id="artist-input">
+					<label for="artist-name">Artist:</label>
+					<input type="text" name="artist-name">
+				</div>
+				<div id="song-name-input">
+					<label for="song-name">Song Name:</label>
+					<input type="text" name="song-name">
+				</div>
+				<div id="genre-input">
+					<label for="genre">Genre:</label>
+					<input type="text" name="genre">
+				</div>
+			</div>
+			<div class=upload-buttons>
+				<div class="select-song-button">
+					<button id="select-song">Select Song</button>
+				</div>
+				<div class="select-image-button">
+					<button id="select-image">Select Image</button>
+				</div>
+			</div>
+			<div class="upload-button">
+				<button id="upload-all-items">Upload</button>
+			</div>
+		`;
+	}
+	
+	render() {
+		return this.container;
+	}
+}
