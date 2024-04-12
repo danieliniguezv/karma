8bd7b5968594bb392c5e14cea6bae473040cbc14
Daniel Iñiguez <danieliniguezv@MacBook-Pro-de-Daniel.local>
2024-04-11 18:34:08 -0600

    fix: class names and data page

--- frontend/src/components/upload.js
+++ frontend/src/components/upload.js
@@ -4,31 +4,33 @@ export default class UploadComponent {
 		this.container.classList.add('upload');
 		this.container.setAttribute('data-page', page);
 		this.container.innerHTML = `
-		<div class="login" data-page="${page}"></div>
+		<div class="log-in" data-page="${page}">
+			<img src="../src/images/new-logo-website-1.svg" id="logo-log-in" data-page="${page}">
+		</div>
 			<div class="input-fields" data-page="${page}">
-				<div id="artist-input">
+				<div class="input-field">
 					<label for="artist-name">Artist:</label>
 					<input type="text" name="artist-name">
 				</div>
-				<div id="song-name-input">
+				<div class="input-field">
 					<label for="song-name">Song Name:</label>
 					<input type="text" name="song-name">
 				</div>
-				<div id="genre-input">
+				<div class="input-field">
 					<label for="genre">Genre:</label>
 					<input type="text" name="genre">
 				</div>
 			</div>
-			<div class=upload-buttons>
-				<div class="select-song-button">
-					<button id="select-song">Select Song</button>
+			<div class=upload-buttons data-page="${page}">
+				<div class="button" id="select-song-button" data-page="${page}">
+					<p>Select Song</p>
 				</div>
-				<div class="select-image-button">
-					<button id="select-image">Select Image</button>
+				<div class="button" id="select-image-button" data-page="${page}">
+					<p>Select Image</p>
 				</div>
 			</div>
-			<div class="upload-button">
-				<button id="upload-all-items">Upload</button>
+			<div class="button" id="upload-button">
+				<p>Upload</p>
 			</div>
 		`;
 	}
